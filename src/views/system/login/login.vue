62e5ee7b566368dec0ce794751e38fb096db2d32
zhanjianwen <zhanjianwen@zhanjianwendeMacBook-Pro.local>
2019-10-09 23:22:00 +0800

    init commit

---
+++ src/views/system/login/login.vue
@@ -0,0 +1,25 @@
+<!-- <template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+
+@Component({
+  components: {
+    HelloWorld,
+  },
+})
+export default class Home extends Vue {}
+</script> -->
+<template>
+  <div>login</div>
+</template>
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+export default class Login extends Vue {}
+</script>
