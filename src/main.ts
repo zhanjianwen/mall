2e87eb120a6b578017e985570b20aeffb60b5ce9
占建文 <zhanjianwen163@163.com>
2019-10-11 13:42:10 +0800

    修改

--- src/main.ts
+++ src/main.ts
@@ -2,19 +2,18 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './stores';
-import api from './apis/index';
+// import api from './apis/index';
 import moment from 'moment';
 import _ from 'lodash';
 // import FastClick from 'fastclick';
 // FastClick['attach'](document.body);
 declare global {
   interface Window {
-    $api: any;
     $moment: any;
     $_: any;
   }
 }
-window.$api = api;
+// window.$api = api;
 window.$moment = moment;
 window.$_ = _;
 Vue.config.productionTip = false;
--- src/router.ts
+++ src/router.ts
@@ -1,6 +1,6 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Login from './views/system/login/login.vue';
+import Login from '@/views/system/login/login.vue';
 Vue.use(Router);
 
 export default new Router({
