ccc8df689fe8524a0614a144c4fd223a61280e5f
占建文 <zhanjianwen163@163.com>
2019-10-24 11:29:57 +0800

    xg

--- src/db/home/navList.ts
+++ src/db/home/navList.ts
@@ -1,5 +1,6 @@
-createdTime: Number;//创建事件
+createdTime: Number;//创建时间
 name: String;//名称
 type: String;//类型
 id: String;//编号
-iconUrl: String;//图标地址 
+iconUrl: String;//图标地址
+updatedTime: Number;//更新时间
